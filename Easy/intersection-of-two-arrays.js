c93104e5adf43dfa6a39249018ae6c83ed1ca7ac
Renjen Jacob <76827550+renjenjacob@users.noreply.github.com>
2023-07-31 16:05:40 -0400

    Add files via upload

---
+++ Easy/intersection-of-two-arrays.js
@@ -0,0 +1,42 @@
+//Given two integer arrays nums1 and nums2, return an array of their intersection. 
+//Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+
+//Examples
+
+//Input: nums1 = [1,2,2,1], nums2 = [2,2]
+//Output: [2,2]
+
+//Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//Output: [4,9]
+//Explanation: [9,4] is also accepted.
+
+//Constraints:
+//1 <= nums1.length, nums2.length <= 1000
+//0 <= nums1[i], nums2[i] <= 1000
+
+//Solution
+
+var intersect = function(nums1, nums2) {
+	// sort the arrays
+    nums1.sort((a,b) => a-b), nums2.sort((a,b) => a-b);
+	
+    let i = 0,
+        j = 0,
+        result = [];
+    
+    while(i<nums1.length && j<nums2.length) {
+        if(nums1[i] < nums2[j]){
+            i++;
+        } else if(nums1[i] > nums2[j]) {
+            j++;
+        } else {
+            result.push(nums1[i]);
+            i++;
+            j++;
+        }
+    }
+    
+    return result;
+}
+
+
