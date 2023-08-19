fcfba373e3be59137fefd04e33b9db9ff2d367ad
Renjen Jacob <76827550+renjenjacob@users.noreply.github.com>
2023-08-18 23:39:48 -0400

    Add files via upload

---
+++ Easy/power-of-two.js
@@ -0,0 +1,30 @@
+//Given an integer n, return true if it is a power of two. Otherwise, return false.
+//An integer n is a power of two, if there exists an integer x such that n == 2x.
+
+//Examples
+
+//Input: n = 1
+//Output: true
+//Explanation: 20 = 1
+
+//Input: n = 16
+//Output: true
+//Explanation: 24 = 16
+
+//Input: n = 3
+//Output: false
+
+//Constraints:
+//-231 <= n <= 231 - 1
+
+//Solution
+
+var isPowerOfTwo = function(n) {
+    let x = 0;
+    while (2**x < n) {
+        x++;
+    }
+    return 2**x === n;
+}
+
+
