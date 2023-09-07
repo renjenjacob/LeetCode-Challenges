cf7567dac4e4cde717e61af7208d844aff60af90
Renjen Jacob <76827550+renjenjacob@users.noreply.github.com>
2023-09-07 19:15:45 -0400

    Add files via upload

---
+++ Easy/perfect-number.js
@@ -0,0 +1,34 @@
+//A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
+//A divisor of an integer x is an integer that can divide x evenly.
+//Given an integer n, return true if n is a perfect number, otherwise return false.
+
+//Examples
+
+//Input: num = 28
+//Output: true
+//Explanation: 28 = 1 + 2 + 4 + 7 + 14
+//1, 2, 4, 7, and 14 are all divisors of 28.
+
+//Input: num = 7
+//Output: false
+
+//Constraints:
+//1 <= num <= 108
+
+//Solution
+
+var checkPerfectNumber = function(num) {
+    if(num == 1) return false;
+    let sum = 1; 
+    let n = Math.sqrt(num);
+
+    for(let i = 2; i <= n; i++){
+        if(num%i === 0){
+            sum += i;
+            if(i !== n) sum += num/i;
+        }
+    }
+    
+    return sum === num ? true : false;
+}
+
