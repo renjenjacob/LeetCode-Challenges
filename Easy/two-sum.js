90e90d74ae9a4f129ead424f0879f47f5634271f
Renjen Jacob <76827550+renjenjacob@users.noreply.github.com>
2023-11-29 11:21:31 -0500

    Update two-sum.js

--- Easy/two-sum.js
+++ Easy/two-sum.js
@@ -39,3 +39,19 @@ var twoSum = function(nums, target) {
         i++;
     }
 }
+
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        #O(n)
+        seen = {}
+        for i in range(len(nums)):
+            diff = target-nums[i]
+            if diff in seen:
+                return [seen[diff],i]
+            else:
+                seen[nums[i]] = i
